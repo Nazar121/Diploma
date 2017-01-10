1f8630439dacf59bf668385387e539f7d3afa721
Nazar <BurbaNazar98@gmail.com>
2017-01-10 21:35:18 +0200

    Mobile First: header

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -2,4 +2,8 @@ angular.module('mainCtrl',[])
     .controller('mainCtrl',mainCtrl)
 mainCtrl.$inject=['Factory','$http'];           
 
-function mainCtrl(Factory,$http){};
+function mainCtrl(Factory,$http){
+    this.menu = function(){
+        $('.header__menu').toggle(200);
+    };
+};
